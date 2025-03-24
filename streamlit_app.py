de6f47b733cbfa054c4d4de08a2c33e5e8599193
RylVibra <168001434+RylVibra@users.noreply.github.com>
2025-03-24 14:17:08 +0000

    reset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,16 @@
 import streamlit as st
-import streamlit as st
 from streamlit_qrcode_scanner import qrcode_scanner
 
 
 st.header("QR Code Scanner")
 
-enable = st.checkbox("Enable camera")
+# camera = st.camera_input("Show QR code")
 
-camera = st.camera_input("Show QR code")
+def run_scanner():
+    qr_code = qrcode_scanner()
+
+if st.button("Scan", key="QRscan"):
+    qr_code = qrcode_scanner()
+    if qr_code:
+        st.write(f"# {qr_code}")
+
